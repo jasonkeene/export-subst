5845945ec6263296e142b446d37a82e114413df4
Jason Keene <jasonkeene@gmail.com>
2018-09-27 14:40:31 -0600

    fail to build when export-subst is applied

--- subst.go
+++ subst.go
@@ -1,13 +1,7 @@
-// Package subst tests if export substitution occured.
+// Package subst will fail to build if export substitution occurred.
 package subst
 
-const (
-	fixture      = "$Format:%h$"
-	expectedData = "\x24\x46\x6f\x72\x6d\x61\x74\x3a\x25\x68\x24"
-)
+type got = [len("$Format:%h$")]byte
+type want = [len("Format:%h") + 2]byte
 
-// Enabled returns true if export-subst was enabled when the zip file was
-// created for this module.
-func Enabled() bool {
-	return fixture != expectedData
-}
+var check got = want{}
